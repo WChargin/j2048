0865210f086f4f4af0a8ef7e472beb5fe244f277
WChargin <wchargin@gmail.com>
2014-03-21 13:28:10 -0700

    Implement maximum board size checking
    The BoardLocation class now checks for maximum board size, in addition
    to the previous check for nonnegativity.

--- j2048/src/j2048/BoardLocation.java
+++ j2048/src/j2048/BoardLocation.java
@@ -10,6 +10,11 @@ package j2048;
 public final class BoardLocation {
 
 	/**
+	 * The side length of the grid.
+	 */
+	public static final int BOARD_SIZE = 4;
+
+	/**
 	 * The {@code x}-position of this location in the grid. The leftmost cells
 	 * in the grid have {@code x}-position {@code 0}.
 	 */
@@ -23,20 +28,26 @@ public final class BoardLocation {
 
 	/**
 	 * Creates a location with the given coordinates. The coordinates must be
-	 * non-negative (greater than or equal to zero).
+	 * non-negative (greater than or equal to zero) and less than the board size
+	 * of {@value #BOARD_SIZE}.
 	 * 
 	 * @param x
 	 *            the {@code x}-position for this tile
 	 * @param y
 	 *            the {@code y}-position for this tile
 	 * @throws IllegalArgumentException
-	 *             if {@code x < 0} or {@code y < 0}
+	 *             if {@code x} or {@code y} is negative, or greater than or
+	 *             equal to {@value #BOARD_SIZE}
 	 */
 	public BoardLocation(int x, int y) throws IllegalArgumentException {
 		if (x < 0) {
 			throw new IllegalArgumentException("x is negative: " + x);
 		} else if (y < 0) {
 			throw new IllegalArgumentException("y is negative: " + y);
+		} else if (x >= BOARD_SIZE) {
+			throw new IllegalArgumentException("x is too big: " + x);
+		} else if (y >= BOARD_SIZE) {
+			throw new IllegalArgumentException("y is too big: " + y);
 		}
 		this.x = x;
 		this.y = y;
