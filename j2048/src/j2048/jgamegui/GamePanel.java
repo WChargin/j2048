4c1232ba8546d871bc195c8db35e9fadb39d8df3
WChargin <wchargin@gmail.com>
2014-03-25 17:37:36 -0700

    Add functionality and Javadoc to score displays

--- j2048/src/j2048/jgamegui/GamePanel.java
+++ j2048/src/j2048/jgamegui/GamePanel.java
@@ -16,6 +16,110 @@ import jgame.GMessage;
 public class GamePanel extends GContainer {
 
 	/**
+	 * A score display component with a label and a mutable value.
+	 * 
+	 * @author William Chargin
+	 * 
+	 */
+	private static class ScoreDisplay extends GContainer {
+
+		/**
+		 * Creates a message to be used as a label for the score.
+		 * 
+		 * @param text
+		 *            the text of the label
+		 * @return the label message
+		 */
+		private static GMessage createLabelMessage(String text) {
+			final GMessage label = new GMessage();
+			label.setColor(J2048.LIGHT_TEXT);
+			label.setText(text.toUpperCase());
+			label.setSize(90, 30);
+			label.setFontSize(16);
+			label.setFontStyle(Font.BOLD);
+			label.setAlignmentX(1);
+			label.setAlignmentY(0.5);
+			label.setAnchorTopLeft();
+			return label;
+		}
+
+		/**
+		 * Creates a message representing a score.
+		 * 
+		 * @param value
+		 *            the initial value for the score message
+		 * @return the score message
+		 */
+		private static GMessage createScoreMessage(int value) {
+			final GMessage score = new GMessage();
+			score.setColor(Color.WHITE);
+			score.setText(Integer.toString(value));
+			score.setSize(100, 30);
+			score.setFontSize(22);
+			score.setFontStyle(Font.BOLD);
+			score.setAlignmentX(0);
+			score.setAlignmentY(0.5);
+			score.setAnchorTopLeft();
+			return score;
+		}
+
+		/**
+		 * The actual integer value of the score.
+		 */
+		private int scoreValue;
+
+		/**
+		 * The message displaying the score value.
+		 */
+		private final GMessage scoreMessage;
+
+		/**
+		 * Creates a {@code ScoreDisplay} initialized to the given title and a
+		 * score of {@code 0}.
+		 * 
+		 * @param title
+		 *            the title of this display
+		 */
+		public ScoreDisplay(String title) {
+			setAnchorTopLeft();
+			setSize(200, 30);
+			GMessage label = createLabelMessage(title);
+			addAt(label, 0, 0);
+
+			scoreMessage = createScoreMessage(0);
+			addAt(scoreMessage, 100, 0);
+		}
+
+		/**
+		 * Gets the current score.
+		 * 
+		 * @return the current score
+		 */
+		public int getScore() {
+			return scoreValue;
+		}
+
+		@Override
+		public void paint(Graphics2D g) {
+			g.setColor(J2048.MAIN_COLOR);
+			g.fillRoundRect(0, 0, getIntWidth(), getIntHeight(), 3, 3);
+			super.paint(g);
+		}
+
+		/**
+		 * Sets the score displayed in this {@code ScoreDisplay}.
+		 * 
+		 * @param newScore
+		 *            the new displayed score
+		 */
+		public void setScore(int newScore) {
+			scoreValue = newScore;
+			scoreMessage.setText(Integer.toString(scoreValue));
+		}
+
+	}
+
+	/**
 	 * The message representing the user's current score.
 	 */
 	private final ScoreDisplay scoreValue;
@@ -51,57 +155,27 @@ public class GamePanel extends GContainer {
 		addAt(grid, 0, 100);
 	}
 
-	private static class ScoreDisplay extends GContainer {
+	/**
+	 * Updates the "best score" display to reflect the new best score.
+	 * 
+	 * @param newBestScore
+	 *            the new best score
+	 */
+	public void setBestScore(int newBestScore) {
+		bestValue.setScore(newBestScore);
+	}
 
-		private final GMessage score;
-
-		public ScoreDisplay(String title) {
-			setAnchorTopLeft();
-			setSize(150, 30);
-			GMessage label = createLabelMessage(title);
-			addAt(label, 0, 0);
-
-			score = createScoreMessage(0);
-			addAt(score, 100, 0);
+	/**
+	 * Updates the "score" display to reflect the new score.
+	 * 
+	 * @param newScore
+	 *            the new score
+	 */
+	public void setScore(int newScore) {
+		scoreValue.setScore(newScore);
+		if (newScore > bestValue.getScore()) {
+			setBestScore(newScore);
 		}
-
-		@Override
-		public void paint(Graphics2D g) {
-			g.setColor(J2048.MAIN_COLOR);
-			g.fillRoundRect(0, 0, getIntWidth(), getIntHeight(), 3, 3);
-			super.paint(g);
-		}
-
-		public void setScore(int newScore) {
-			score.setText(Integer.toString(newScore));
-		}
-
-		private static GMessage createScoreMessage(int value) {
-			final GMessage score = new GMessage();
-			score.setColor(Color.WHITE);
-			score.setText(Integer.toString(value));
-			score.setSize(100, 30);
-			score.setFontSize(24);
-			score.setFontStyle(Font.BOLD);
-			score.setAlignmentX(0);
-			score.setAlignmentY(0.5);
-			score.setAnchorTopLeft();
-			return score;
-		}
-
-		private static GMessage createLabelMessage(String text) {
-			final GMessage label = new GMessage();
-			label.setColor(J2048.LIGHT_TEXT);
-			label.setText(text.toUpperCase());
-			label.setSize(90, 30);
-			label.setFontSize(18);
-			label.setFontStyle(Font.BOLD);
-			label.setAlignmentX(1);
-			label.setAlignmentY(0.5);
-			label.setAnchorTopLeft();
-			return label;
-		}
-
 	}
 
 }
