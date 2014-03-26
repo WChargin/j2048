1090f2b94b921f7ef510f90d8350822d9ad74c85
WChargin <wchargin@gmail.com>
2014-03-25 20:13:18 -0700

    Switch to applet (Game -> GameApplet extension)

--- j2048/src/j2048/jgamegui/J2048.java
+++ j2048/src/j2048/jgamegui/J2048.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 
 import jgame.GContainer;
 import jgame.GRootContainer;
-import jgame.Game;
+import jgame.GameApplet;
 
 /**
  * The driver class for a 2048 game.
@@ -12,7 +12,12 @@ import jgame.Game;
  * @author William Chargin
  * 
  */
-public class J2048 extends Game {
+public class J2048 extends GameApplet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	public static final Color MAIN_COLOR = new Color(187, 173, 160);
 	public static final Color TEXT_COLOR = new Color(119, 110, 101);
@@ -20,20 +25,16 @@ public class J2048 extends Game {
 	public static final Color LIGHT_TEXT = new Color(238, 228, 218);
 
 	public J2048() {
-		setTargetFPS(60);
+		core.setTargetFPS(60);
 
 		GRootContainer root = new GRootContainer(LIGHT_COLOR);
-		setRootContainer(root);
+		core.setRootContainer(root);
 
 		GContainer container = new GContainer();
 		GamePanel game = new GamePanel();
 		container.setSize(game.getWidth() + 50, game.getHeight() + 50);
 		container.addAtCenter(game);
 		root.add(container);
-	}
-
-	public static void main(String[] args) {
-		new J2048().startGame("2048");
 	}
 
 }
